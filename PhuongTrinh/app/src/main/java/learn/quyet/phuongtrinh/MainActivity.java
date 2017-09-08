0062aa60afc3f82b082ec89708a5643d0e7eb8d5
COUPLEQ\Quyet <quyetdv155214@gmail.com>
2017-09-08 16:36:58 +0700

    ex giai phuong trinh 2

--- PhuongTrinh/app/src/main/java/learn/quyet/phuongtrinh/MainActivity.java
+++ PhuongTrinh/app/src/main/java/learn/quyet/phuongtrinh/MainActivity.java
@@ -2,13 +2,28 @@ package learn.quyet.phuongtrinh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
+    @BindView(R.id.tvA) TextView tvA;
+    @BindView(R.id.tvB) TextView tvB;
+    @BindView(R.id.txtA) TextView txtA;
+    @BindView(R.id.txtB) TextView txtB;
+    @BindView(R.id.txtC) TextView txtC;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setupUI();
+    }
+    private void setupUI(){
+        tvA.setText(Html.fromHtml("X<sup>2</sup>"));
+        tvB.setText("X");
     }
 
 }
